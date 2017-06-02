19c5937143ed857b3c4ce565fdbc5ff21e1d7692
Hugo Correa <hugosalescorrea@gmail.com>
2017-06-02 08:42:58 -0300

    Added sparkContext

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,18 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
 object Main {
     def main(args: Array[String]) : Unit = {
-        println("Hello World")
+
+        val sparkConf = (new SparkConf()).setAppName("SparkTweets").setMaster("local[4]")
+        val sc = new SparkContext(sparkConf)
+
+        /**
+        *
+        * As chamadas para os comandos ficam aqui antes do sc.stop
+        *
+        */
+
+        sc.stop()
     }
 }
