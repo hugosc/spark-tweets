e1c3e5193d62276783bf0f01b0ac08fa2df67b74
Hugo Correa <hugosalescorrea@gmail.com>
2017-06-04 11:55:14 -0300

    Question 2 done

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,40 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd._
 import tweets_sanitize._
+import java.time._
+
 
 object Main {
+
+    def topHashtagsPerDay(dt :RDD[Tweet]) : RDD[(Long, Array[(String, Int)])]= {
+        val res1 = dt flatMap { t =>
+            t.content.split(" ") filter (_.startsWith("#")) map { c =>
+                ((t.datetime.toLocalDate.toEpochDay, c), 1)
+            }
+        }
+        val res2 = res1 reduceByKey (_+_) map {
+            case ((l, s), c) => (l, (s, c))
+        }
+
+        res2.groupByKey map { case (k, i) => (k, i.toArray.sortWith(_._2 > _._2)) }
+    }
+
+    def printTop10Hashtags(agg: RDD[(Long, Array[(String, Int)])]) : Unit = {
+        agg foreach {case (d, i) =>
+            {println(LocalDate.ofEpochDay(d))
+            println("+++++++++++++++++++++")
+            i.take(10) foreach {println(_)}
+            println("+++++++++++++++++++++")}
+        }
+    }
+
+
     def main(args: Array[String]) : Unit = {
 
         val sparkConf = (new SparkConf()).setAppName("SparkTweets").setMaster("local[4]")
         val sc = new SparkContext(sparkConf)
+        sc.setLogLevel("WARN")
 
         /**
         *
@@ -16,7 +44,13 @@ object Main {
 
         val dt = sc.textFile("data/dataset.tsv")
         val tweets = dt map (TweetUtils.rawToTweet)
-        println(tweets.first)
+
+        println("--------- REPORT ------------")
+
+        val res = topHashtagsPerDay(tweets)
+        printTop10Hashtags(res)
+
+        println("-----------------------------")
 
         sc.stop()
     }
